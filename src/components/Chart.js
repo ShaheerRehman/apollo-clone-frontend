d9a9db6a04603155dfd207144a7fe4df4c214487
ShaheerRehman <shaheerr7@gmail.com>
2022-09-07 04:14:00 +0500

    basic set up and data fetching

---
+++ src/components/Chart.js
@@ -0,0 +1,97 @@
+import * as React from "react";
+import { useTheme } from "@mui/material/styles";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import { Box, Typography, Container, Button } from "@mui/material";
+import styled from "@emotion/styled";
+import { fontSize } from "@mui/system";
+import { red } from "@mui/material/colors";
+
+// Generate Sales Data
+
+const Circle = styled(CheckCircleIcon)({
+  color: "#02C797",
+  fontSize: "20px",
+  marginRight: "3px",
+});
+
+const Title = styled(Typography)({
+  fontSize: "15px",
+  fontWeight: 600,
+  marginBottom: "2px",
+});
+
+const MyBox = styled(Box)({
+  display: "flex",
+  alignItems: "center",
+});
+
+const Options = styled(Typography)({
+  color: "#5D6A7E",
+  fontSize: "14px",
+  marginRight: "28px",
+});
+
+export default function Chart() {
+  const theme = useTheme();
+
+  return (
+    <React.Fragment>
+      {/* <Container maxWidth="sm" sx={{ marginLeft: 0 }}> */}
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            flexDirection: "column",
+            // backgroundColor: "red",
+          }}
+        >
+          <Title>Need more pipeline? Upgrade now for advanced features.</Title>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+            }}
+          >
+            <MyBox>
+              <Circle />
+              <Options>Advanced Filters</Options>
+            </MyBox>
+            <MyBox>
+              <Circle />
+
+              <Options>Find mobile numbers</Options>
+            </MyBox>
+            <MyBox>
+              <Circle />
+
+              <Options>Push to CRM</Options>
+            </MyBox>
+          </Box>
+        </Box>
+        <Button
+          sx={{
+            width: "auto",
+            height: "32px",
+            borderRadius: "4px",
+            padding: "0px 12px",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            fontSize: "14px",
+          }}
+          variant="contained"
+        >
+          View Plans
+        </Button>
+      </Box>
+      {/* </Container> */}
+    </React.Fragment>
+  );
+}
---
+++ src/components/Dashboard.js
@@ -0,0 +1,175 @@
+import * as React from "react";
+import { styled, createTheme, ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import MuiDrawer from "@mui/material/Drawer";
+import Box from "@mui/material/Box";
+import MuiAppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import List from "@mui/material/List";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import Badge from "@mui/material/Badge";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+import Link from "@mui/material/Link";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import { mainListItems, secondaryListItems } from "./ListItems";
+import Chart from "./Chart";
+import Orders from "./Orders";
+import index from "../assets/images/index.svg";
+import { Avatar } from "@mui/material";
+import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
+
+function Copyright(props) {
+  return (
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
+const drawerWidth = 240;
+
+const AppBar = styled(MuiAppBar)(({ theme }) => ({
+  backgroundColor: "white",
+  color: "black",
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(["width", "margin"], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+}));
+
+const Drawer = styled(MuiDrawer, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme }) => ({
+  "& .MuiDrawer-paper": {
+    position: "relative",
+    whiteSpace: "nowrap",
+    width: drawerWidth,
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    boxSizing: "border-box",
+  },
+}));
+
+const NavItem = styled(Typography)({
+  marginLeft: "30px",
+  fontSize: "17px",
+});
+
+const mdTheme = createTheme();
+
+function DashboardContent() {
+  const open = true;
+
+  return (
+    <ThemeProvider theme={mdTheme}>
+      <Box sx={{ display: "flex" }}>
+        <CssBaseline />
+        <AppBar position="absolute" open={open}>
+          <Toolbar
+            sx={{
+              pr: "24px", // keep right padding when drawer closed
+            }}
+          >
+            <img src={index} alt="logo" />
+            <Box sx={{ flexGrow: 1 }}>
+              <Box sx={{ display: "flex" }}>
+                <NavItem>Home</NavItem>
+                <NavItem>Search</NavItem>
+                <NavItem>Engage</NavItem>
+                <NavItem>Enrich</NavItem>
+              </Box>
+            </Box>
+
+            <IconButton color="inherit">
+              <Badge badgeContent={4} color="secondary">
+                <NotificationsNoneOutlinedIcon
+                  sx={{ height: "30px", width: "30px" }}
+                />
+              </Badge>
+            </IconButton>
+            <Avatar
+              sx={{ ml: "15px", height: "30px", width: "30px" }}
+              alt="Remy Sharp"
+              src=""
+            />
+          </Toolbar>
+        </AppBar>
+        <Drawer variant="permanent" open={open}>
+          <Toolbar
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "flex-end",
+              px: [1],
+            }}
+          ></Toolbar>
+          <Divider />
+          <List component="nav">
+            {mainListItems}
+            <Divider sx={{ my: 1 }} />
+            {secondaryListItems}
+          </List>
+        </Drawer>
+        <Box
+          component="main"
+          sx={{
+            backgroundColor: (theme) =>
+              theme.palette.mode === "light"
+                ? theme.palette.grey[100]
+                : theme.palette.grey[900],
+            flexGrow: 1,
+            height: "100vh",
+            overflow: "auto",
+          }}
+        >
+          <Toolbar />
+          <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+            <Grid container spacing={3}>
+              {/* Chart */}
+              <Grid item xs={12}>
+                <Paper
+                  sx={{
+                    p: 2,
+                    display: "flex",
+                    flexDirection: "column",
+                    height: 80,
+                  }}
+                >
+                  <Chart />
+                </Paper>
+              </Grid>
+              {/* Recent Orders */}
+              <Grid item xs={12}>
+                <Paper sx={{ p: 2, display: "flex", flexDirection: "column" }}>
+                  <Orders />
+                </Paper>
+              </Grid>
+            </Grid>
+            <Copyright sx={{ pt: 4 }} />
+          </Container>
+        </Box>
+      </Box>
+    </ThemeProvider>
+  );
+}
+
+export default function Dashboard() {
+  return <DashboardContent />;
+}
