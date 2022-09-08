69814d4d77902285e869afc470bda8f4f39ce1cd
ShaheerRehman <shaheerr7@gmail.com>
2022-09-09 03:07:24 +0500

    location functionality improvised

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,17 +2,16 @@ import * as React from "react";
 import { styled, createTheme, ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import MuiDrawer from "@mui/material/Drawer";
+import LocationOnOutlinedIcon from "@mui/icons-material/LocationOnOutlined";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
-import ListSubheader from "@mui/material/ListSubheader";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import PeopleIcon from "@mui/icons-material/People";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 import BarChartIcon from "@mui/icons-material/BarChart";
 import LayersIcon from "@mui/icons-material/Layers";
 import SearchIcon from "@mui/icons-material/Search";
-import AssignmentIcon from "@mui/icons-material/Assignment";
 import Search from "./Search";
 import Box from "@mui/material/Box";
 import MuiAppBar from "@mui/material/AppBar";
@@ -26,12 +25,13 @@ import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
 import Link from "@mui/material/Link";
-import { mainListItems, secondaryListItems } from "./ListItems";
+import { secondaryListItems } from "./ListItems";
 import Chart from "./Chart";
 import Orders from "./Orders";
 import index from "../assets/images/index.svg";
-import { Avatar } from "@mui/material";
+import { alpha, Avatar, InputBase } from "@mui/material";
 import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
+import { useNavigate } from "react-router-dom";
 
 function Copyright(props) {
   return (
@@ -83,13 +83,54 @@ const NavItem = styled(Typography)({
   fontSize: "17px",
 });
 
+const SearchDiv = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  // marginLeft: 8,
+  height: "35px",
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(1),
+    width: "auto",
+  },
+}));
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 1),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  "& .MuiInputBase-input": {
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      width: "12ch",
+      "&:focus": {
+        width: "20ch",
+      },
+    },
+  },
+}));
 const mdTheme = createTheme();
 
 function DashboardContent({ pData }) {
   const open = true;
   // React.useEffect(() => {}, [props.data]);
   const [openLocation, setOpenLocation] = React.useState(false);
-
+  const navigate = useNavigate();
   return (
     <ThemeProvider theme={mdTheme}>
       <Box sx={{ display: "flex" }}>
@@ -100,10 +141,20 @@ function DashboardContent({ pData }) {
               pr: "24px", // keep right padding when drawer closed
             }}
           >
-            <img src={index} alt="logo" />
+            <img
+              style={{ cursor: "pointer" }}
+              src={index}
+              alt="logo"
+              onClick={() => navigate("/")}
+            />
             <Box sx={{ flexGrow: 1 }}>
               <Box sx={{ display: "flex" }}>
-                <NavItem>Home</NavItem>
+                <NavItem
+                  style={{ cursor: "pointer" }}
+                  onClick={() => navigate("/")}
+                >
+                  Home
+                </NavItem>
                 <NavItem>Search</NavItem>
                 <NavItem>Engage</NavItem>
                 <NavItem>Enrich</NavItem>
@@ -135,9 +186,24 @@ function DashboardContent({ pData }) {
           ></Toolbar>
           <Divider />
           <List component="nav">
+            <SearchDiv>
+              <SearchIconWrapper>
+                <ListItemIcon sx={{ paddingBottom: 0 }}>
+                  <SearchIcon />
+                </ListItemIcon>
+              </SearchIconWrapper>
+
+              <StyledInputBase
+                // onChange={(e) => setData({ search: e.target.value })}
+                placeholder="Search…"
+                inputProps={{ "aria-label": "search" }}
+              />
+            </SearchDiv>
+            <Divider sx={{ my: 1 }} />
+
             <ListItemButton onClick={() => setOpenLocation(!openLocation)}>
               <ListItemIcon>
-                <SearchIcon />
+                <LocationOnOutlinedIcon />
               </ListItemIcon>
               <ListItemText primary="Location" />
               <ListItemIcon
@@ -156,32 +222,20 @@ function DashboardContent({ pData }) {
                 <Search />
               </Box>
             ) : null}
-            <ListItemButton>
-              <ListItemIcon>
+            {/* <ListItemButton> */}
+            {/* <ListItemIcon>
                 <ShoppingCartIcon />
               </ListItemIcon>
               <ListItemText primary="Orders" />
             </ListItemButton>
             <ListItemButton>
               <ListItemIcon>
-                <PeopleIcon />
-              </ListItemIcon>
-              <ListItemText primary="Customers" />
-            </ListItemButton>
-            <ListItemButton>
-              <ListItemIcon>
-                <BarChartIcon />
-              </ListItemIcon>
-              <ListItemText primary="Reports" />
-            </ListItemButton>
-            <ListItemButton>
-              <ListItemIcon>
                 <LayersIcon />
               </ListItemIcon>
               <ListItemText primary="Integrations" />
-            </ListItemButton>
+            </ListItemButton>*/}
             <Divider sx={{ my: 1 }} />
-            {secondaryListItems}
+            {/* {secondaryListItems}  */}
           </List>
         </Drawer>
         <Box
--- src/components/ListItems.js
+++ src/components/ListItems.js
@@ -3,53 +3,7 @@ import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import ListSubheader from "@mui/material/ListSubheader";
-import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
-import PeopleIcon from "@mui/icons-material/People";
-import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
-import BarChartIcon from "@mui/icons-material/BarChart";
-import LayersIcon from "@mui/icons-material/Layers";
-import SearchIcon from "@mui/icons-material/Search";
 import AssignmentIcon from "@mui/icons-material/Assignment";
-import Search from "./Search";
-
-export const mainListItems = (
-  <React.Fragment>
-    <ListItemButton>
-      <ListItemIcon>
-        <SearchIcon />
-      </ListItemIcon>
-      <ListItemText primary="Location" />
-      <Search />
-      <ListItemIcon>
-        <ArrowDropDownIcon />
-      </ListItemIcon>
-    </ListItemButton>
-    <ListItemButton>
-      <ListItemIcon>
-        <ShoppingCartIcon />
-      </ListItemIcon>
-      <ListItemText primary="Orders" />
-    </ListItemButton>
-    <ListItemButton>
-      <ListItemIcon>
-        <PeopleIcon />
-      </ListItemIcon>
-      <ListItemText primary="Customers" />
-    </ListItemButton>
-    <ListItemButton>
-      <ListItemIcon>
-        <BarChartIcon />
-      </ListItemIcon>
-      <ListItemText primary="Reports" />
-    </ListItemButton>
-    <ListItemButton>
-      <ListItemIcon>
-        <LayersIcon />
-      </ListItemIcon>
-      <ListItemText primary="Integrations" />
-    </ListItemButton>
-  </React.Fragment>
-);
 
 export const secondaryListItems = (
   <React.Fragment>
